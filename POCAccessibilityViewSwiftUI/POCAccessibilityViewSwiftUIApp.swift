8df9547d3476023e4787cc118600022d11e309a6
Lucas Barbosa de Oliveira <lucasbarbosa2807@gmail.com>
2023-02-13 08:48:26 -0300

    Initial Commit

---
+++ POCAccessibilityViewSwiftUI/POCAccessibilityViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  POCAccessibilityViewSwiftUIApp.swift
+//  POCAccessibilityViewSwiftUI
+//
+//  Created by Lucas Barbosa de Oliveira on 13/02/23.
+//
+
+import SwiftUI
+
+@main
+struct POCAccessibilityViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
